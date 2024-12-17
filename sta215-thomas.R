758bfa917235668a181f94d672ca232c7c6656ec
thomaslily10 <thomasl3@tcnj.edu>
2024-12-16 16:38:29 -0800

    Update sta215-thomas.R

--- sta215-thomas.R
+++ sta215-thomas.R
@@ -35,53 +35,64 @@ summary(linear_relationship)
 plot(dataset_nomissing$state_aid, residuals(linear_relationship))
 
 #Table 1 descriptive statistics 
+#for state aid
 table(dataset$state_aid)
 mean(dataset$state_aid)
 sd(dataset$state_aid)
 
+#for reading score
 table(dataset$reading_score)
 mean(dataset$reading_score)
 sd(dataset$reading_score)
 
-table(dataset$absent_problem)
+#for absent problem
+table(dataset_nomissing$absent_problem)
 
-table(dataset$grades_offered)
+#for students per district
+table(dataset_nomissing$students_per_district)
+mean(dataset_nomissing$students_per_district)
+sd(dataset_nomissing$students_per_district)
+summary(dataset_nomissing$students_per_district)
 
-table(dataset$students_per_district)
-mean(dataset$students_per_district)
-sd(dataset$students_per_district)
+#for student teacher ratio
+table(dataset_nomissing$student_teacher_ratio)
+mean(dataset_nomissing$student_teacher_ratio)
+sd(dataset_nomissing$student_teacher_ratio)
 
-table(dataset$student_teacher_ratio)
-mean(dataset$student_teacher_ratio)
-sd(dataset$student_teacher_ratio)
+#for total students
+table(dataset_nomissing$`total students`)
+mean(dataset_nomissing$`total students`)
+sd(dataset_nomissing$`total students`)
 
-table(dataset$`total students`)
-mean(dataset$`total students`)
-sd(dataset$`total students`)
+#for percentage college admissions rate
+table(dataset_nomissing$percentage_college_admissions_rate)
+mean(dataset_nomissing$percentage_college_admissions_rate)
+sd(dataset_nomissing$percentage_college_admissions_rate)
 
-table(dataset$percentage_college_admissions_rate)
-mean(dataset$percentage_college_admissions_rate)
-sd(dataset$percentage_college_admissions_rate)
+#for percentage of dropout students
+table(dataset_nomissing$percentage_of_dropout_students)
+mean(dataset_nomissing$percentage_of_dropout_students)
+sd(dataset_nomissing$percentage_of_dropout_students)
 
-table(dataset$percentage_of_dropout_students)
-mean(dataset$percentage_of_dropout_students)
-sd(dataset$percentage_of_dropout_students)
+#for number of 4 years experience teachers
+table(dataset_nomissing$number_of_4_years_experience_teachers)
+mean(dataset_nomissing$number_of_4_years_experience_teachers)
+sd(dataset_nomissing$number_of_4_years_experience_teachers)
 
-table(dataset$number_of_4_years_experience_teachers)
-mean(dataset$number_of_4_years_experience_teachers)
-sd(dataset$number_of_4_years_experience_teachers)
+#for teacher average annual salary
+table(dataset_nomissing$teachers_average_annual_salary)
+mean(dataset_nomissing$teachers_average_annual_salary)
+sd(dataset_nomissing$teachers_average_annual_salary)
 
-table(dataset$teachers_average_annual_salary)
-mean(dataset$teachers_average_annual_salary)
-sd(dataset$teachers_average_annual_salary)
+#for spending per student
+table(dataset_nomissing$spending_per_student)
+mean(dataset_nomissing$spending_per_student)
+sd(dataset_nomissing$spending_per_student)
 
-table(dataset$spending_per_student)
-mean(dataset$spending_per_student)
-sd(dataset$spending_per_student)
-
-table(dataset$counseling_services)
-mean(dataset$counseling_services)
-sd(dataset$counseling_services)
+#for counseling services
+table(dataset_nomissing$counseling_services)
+mean(dataset_nomissing$counseling_services)
+sd(dataset_nomissing$counseling_services)
 
 
 
